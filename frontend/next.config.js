a90b04eb021b6c90b2053b6d1f0bf33643961a3d
Marko Sulkunen <sulkunen.marko@gmail.com>
2023-09-25 16:10:20 +0300

    frontend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: "out",
+    reactStrictMode: true,
+    distDir: 'out',
+    output: 'export'
 }
 
 module.exports = nextConfig
