d7e9cb980dab69ddef93b62c997cf03187671646
Marko Sulkunen <sulkunen.marko@gmail.com>
2023-10-20 13:59:19 +0300

    reservations table tamk db

--- TAMK DB Server.session.sql
+++ TAMK DB Server.session.sql
@@ -6,3 +6,16 @@ CREATE TABLE IF NOT EXISTS `users` (
   `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `reservations` (
+  `id` varchar(36) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `service` varchar(60) NOT NULL,
+  `date` timestamp NULL DEFAULT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_unique` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `reservations` (`id`, `email`, `service`, `date`) VALUES (UUID(), 'test1@example.com', 'service1', '2023-10-15 12:00:00');
