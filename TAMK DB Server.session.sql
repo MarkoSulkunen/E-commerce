a8614edd36c3e4ec0f65e4c0e84180e601bac329
Marko Sulkunen <sulkunen.marko@gmail.com>
2023-10-30 12:43:03 +0200

    create reservation

--- TAMK DB Server.session.sql
+++ TAMK DB Server.session.sql
@@ -2,13 +2,18 @@ CREATE TABLE IF NOT EXISTS `users` (
   `id` varchar(36) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(60) NOT NULL,
+  `reset_token` varchar(255) DEFAULT NULL,
+  `reset_token_expiration` timestamp NULL DEFAULT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_unique` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `users` (`id`, `email`, `password`) VALUES (UUID(), 'test@example.com', 'password123');
 
 CREATE TABLE IF NOT EXISTS `reservations` (
-  `id` varchar(36) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(50) NOT NULL,
   `service` varchar(60) NOT NULL,
   `date` timestamp NULL DEFAULT NULL,
@@ -18,4 +23,4 @@ CREATE TABLE IF NOT EXISTS `reservations` (
   UNIQUE KEY `email_unique` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `reservations` (`id`, `email`, `service`, `date`) VALUES (UUID(), 'test1@example.com', 'service1', '2023-10-15 12:00:00');
+INSERT INTO `reservations` (`email`, `service`, `date`) VALUES ('test2@example.com', 'service1', '2023-10-15 12:00:00');
