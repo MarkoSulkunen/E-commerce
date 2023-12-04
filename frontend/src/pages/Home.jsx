3084d302be2059efdc89580d8832bec8db0c5b3a
MarkoSulkunen <112402293+MarkoSulkunen@users.noreply.github.com>
2023-12-04 19:32:52 +0200

    home page service container update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -50,33 +50,23 @@ const Home = () => {
             <h2>Dog Babysitting</h2>
             <p></p>
             <p>
-              Every day we go to long walkies in nature, visiting different dog
-              parks – sometimes for hours – so that your dog gets new
-              experiences and new smells, dogs can run and play! At our hotel
-              your dog can enjoy activities and play with friends at dog parks.
-              You can have a quiet evening or enjoy your holiday in peace!
+              
             </p>
           </div>
           <div className="container">
             {dogPawImage}
-            <h2>Dog mental therapy </h2>
+            <h2>Dog Walking Services </h2>
             <p></p>
             <p>
-              For puppies and dogs that suffer anxieties or are socially
-              challenged, our hotel can be a really good place to learn new
-              skills and behavior in safe environment
+              
             </p>
           </div>
           <div className="container">
             {dogBoneImage}
-            <h2>Dog Care Services </h2>
+            <h2>Dog Boarding Services </h2>
             <p></p>
             <p>
-              Should you need to board your dog at our dog hotel during your
-              holidays, we can do that too – our hotel is luxurious home and
-              cage free. Please bear in mind that all our employees are
-              professional Certified Animal Care takers so your dog is really in
-              good hands 24/7.
+              
             </p>
           </div>
           <div className="container">
@@ -84,8 +74,7 @@ const Home = () => {
             <h2>Medical Care </h2>
             <p></p>
             <p>
-              We provide acupuncture treatment helping with chronic pain and
-              providing natural body balance for your dog.
+             
             </p>
           </div>
           <div className="container">
@@ -93,17 +82,15 @@ const Home = () => {
             <h2>Spa & Wellness </h2>
             <p></p>
             <p>
-              Our "Dog Spa" services offer exclusive massage with essential oil
-              for stress relief and body balance.
+              
             </p>
           </div>
           <div className="container">
             {brushImage}
-            <h2>Salon services</h2>
+            <h2>Dog Grooming Services</h2>
             <p></p>
             <p>
-              Our hotel saloon will offer your pet numerous services from
-              grooming, fur styling to fur extension.
+             
             </p>
           </div>
         </div>
